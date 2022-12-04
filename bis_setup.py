2b3e08cc7c9dfedf9e77fd631f654b0d4569f388
Snorlax <zhangxzheng@gmail.com>
2022-12-04 22:43:21 +0800

    Optimize BUILD file generation

--- bis_setup.py
+++ bis_setup.py
@@ -33,49 +33,6 @@ parser.add_argument('--pre_compile_swift_module', default = True, type = str2boo
 
 args = parser.parse_args()
 
-aquery_args = [
-  'bazel',
-  'aquery',
-  f"mnemonic('(Swift|Objc|Cpp)Compile', inputs('{query_escape(args.file_path)}', deps({args.target})))",
-  '--output=jsonproto',
-  '--include_artifacts=false',
-  '--ui_event_filters=-info',
-  '--noshow_progress',
-  # Disable layering_check during, because it causes large-scale dependence on generated module map files that prevent header extraction before their generation
-      # For more context, see https://github.com/hedronvision/bazel-compile-commands-extractor/issues/83
-      # If https://github.com/clangd/clangd/issues/123 is resolved and we're not doing header extraction, we could try removing this, checking that there aren't erroneous red squigglies squigglies before the module maps are generated.
-      # If Bazel starts supporting modules (https://github.com/bazelbuild/bazel/issues/4005), we'll probably need to make changes that subsume this.
-  '--features=-layering_check',
-  f'--cpu={args.cpu}',
-  f'--compilation_mode={args.compilation_mode}'
-]
-
-# process start
-os.chdir(os.environ["BUILD_WORKSPACE_DIRECTORY"])
-
-print("Run process start !!!")
-print("It may tooks a few minutes depends on the size of target")
-print(f"command = {' '.join(aquery_args)}")
-
-aquery_process = subprocess.run(
-	aquery_args,
-	capture_output= True,
-	encoding=locale.getpreferredencoding(),
-)
-
-print("Run process end !!!")
-
-for line in aquery_process.stderr.splitlines():
-	print(line, file=sys.stderr)
-
-try:
-  parsed_aquery_output = json.loads(aquery_process.stdout, object_hook=lambda d: types.SimpleNamespace(**d))
-  if not hasattr(parsed_aquery_output, 'targets'):
-    parsed_aquery_output.targets = []
-except json.JSONDecodeError:
-  print("Bazel aquery failed. Command:", aquery_args, file=sys.stderr)
-
-targets = ', '.join([f'"{target.label}"' for target in parsed_aquery_output.targets])
 optionals = f'"--compilation_mode={args.compilation_mode} --cpu={args.cpu}"'
 
 template = f"""
@@ -85,7 +42,7 @@ load("@bis//:refresh_launch_json.bzl", "refresh_launch_json")
 refresh_compile_commands(
   name = "refresh_compile_commands",
   targets = [
-    {targets}
+    "{args.target}"
   ],
   optionals = {optionals},
   file_path = "{args.file_path}",
