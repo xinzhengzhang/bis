9ebb524dadf5791d3002f33f794d69580b4e97f1
puer <ihomway@live.com>
2023-05-16 16:22:37 +0800

    [Changed] Optimize the date format of the log

--- plugin/zxz-moe-bis/src/logger.ts
+++ plugin/zxz-moe-bis/src/logger.ts
@@ -4,9 +4,13 @@ let channel: vscode.OutputChannel;
 
 function getFormattedTime() {
     let time = new Date();
-    return `${time.getFullYear()}-${
-        time.getMonth() + 1
-    }-${time.getDate()} ${time.getHours()}:${time.getMinutes()}`;
+    const year = time.getFullYear();
+    const month = (time.getMonth() + 1).toString().padStart(2, '0');
+    const day = time.getDate().toString().padStart(2, '0');
+    const hours = time.getHours().toString().padStart(2, '0');
+    const minutes = time.getMinutes().toString().padStart(2, '0');
+    const seconds = time.getSeconds().toString().padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 function formatSingleMessage(message: any) {
