2cb6466549aedd0d68410c3b6c267a3790ea15b7
puer <ihomway@live.com>
2023-05-16 16:14:51 +0800

    [Changed] Specify the file name of the profile when executing the refresh compile command

--- plugin/zxz-moe-bis/src/launchGenerator.ts
+++ plugin/zxz-moe-bis/src/launchGenerator.ts
@@ -11,7 +11,7 @@ export async function generate() {
     const compilationMode = (await picker.compilationMode()) ?? "dbg";
     const cpu = await cpuProvider.cpu();
 
-    let executionCommands = `${configuration.bazelExecutablePath} run @bis//:setup -- --target ${buildTarget} --optionals \"--compilation_mode=${compilationMode} --cpu="${cpu}" ${configuration.buildOptions}\" --ignore_parsing_targets True`;
+    let executionCommands = `${configuration.bazelExecutablePath} run @bis//:setup -- --target ${buildTarget} --optionals \"--compilation_mode=${compilationMode} --cpu="${cpu}" ${configuration.buildOptions} --profile .bis/bis_bazel_profile\" --ignore_parsing_targets True`;
     executionCommands += `;${configuration.bazelExecutablePath} ${configuration.startupOptions} run //.bis:refresh_launch_json --check_visibility=false --compilation_mode=${compilationMode} --cpu="${cpu}" ${configuration.buildOptions}`;
     const task = new vscode.Task(
         { type: buildType },
